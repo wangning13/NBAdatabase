25134f47ff5d43c00cb0a2b20b590c9ab355d8d4
wangning13 <1287983597@qq.com>
2015-03-10 10:10:17 +0800

    第二次提交

---
+++ src/main/java/po/TeamPO.java
@@ -0,0 +1,34 @@
+package po;
+
+public class TeamPO {
+	String teamName;//球队名称
+	int matches;//比赛场数
+	int fieldGoal;//投篮命中数
+	int fieldGoalAttempts;//投篮出手次数
+	int threePointFieldGoal;//三分命中数
+	int threePointFieldGoalAttempts;//三分出手数
+	int freeThrow;//罚球命中数
+	int freeThrowAttempts;//罚球出手数
+	int offensiveRebound;//进攻篮板数
+	int defensiveRebound;//防守篮板数
+	int backboard;//篮板数
+	int assist;//助攻数
+	int steal;//抢断数
+	int block;//盖帽数
+	int turnOver;//失误数
+	int foul;//犯规数
+	int scoring;//比赛得分
+	double fieldGoalPercentage;//投篮命中率
+	double threePointShotPercentage;//三分命中率
+	double freeThrowPercentage;//罚球命中率
+	double winningPercentage;//胜率
+	int Possessions;//进攻回合
+	double offensiveEfficiency;//进攻效率
+	double defensiveEfficiency;//防守效率
+	double backboardEfficiency;//篮板效率
+	double stealEfficiency;//抢断效率
+	double assitEfficiency;//助攻效率
+	
+	
+
+}
