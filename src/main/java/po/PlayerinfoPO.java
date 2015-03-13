f6eb5168db64cef5d010753e0e3098b8c5f94efa
wangning13 <1287983597@qq.com>
2015-03-13 23:28:56 +0800

    周子懿第13.1次

---
+++ src/main/java/po/PlayerinfoPO.java
@@ -0,0 +1,83 @@
+package po;
+
+public class PlayerinfoPO {
+
+	String name;//球员姓名
+	String number;//球员号码
+	String position;//球员位置
+	String height;//球员身高
+	int weight;//球员体重
+	String birth;//球员生日
+	int age;//球员年龄
+	String exp;//球龄
+	String school;//学校
+	public PlayerinfoPO(String name, String number, String position,
+			String height, int weight, String birth, int age, String exp,
+			String school) {
+		super();
+		this.name = name;
+		this.number = number;
+		this.position = position;
+		this.height = height;
+		this.weight = weight;
+		this.birth = birth;
+		this.age = age;
+		this.exp = exp;
+		this.school = school;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	public String getHeight() {
+		return height;
+	}
+	public void setHeight(String height) {
+		this.height = height;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getExp() {
+		return exp;
+	}
+	public void setExp(String exp) {
+		this.exp = exp;
+	}
+	public String getSchool() {
+		return school;
+	}
+	public void setSchool(String school) {
+		this.school = school;
+	}
+	
+}
