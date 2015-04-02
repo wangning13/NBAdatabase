4d1dddb85664c247b36d6114cabdbba8a986de5f
wangning13 <1287983597@qq.com>
2015-04-02 23:13:23 +0800

    王宁4.2.23.13

--- src/main/java/businesslogicservice/playerblservice/PlayerRankService.java
+++ src/main/java/businesslogicservice/playerblservice/PlayerRankService.java
@@ -9,9 +9,9 @@ import vo.PlayerinfoVO;
 
 public interface PlayerRankService {
 	public ArrayList<String> getAllPlayer(String teamName);//根据球队名称获得所有球员的姓名（数组长度都是15）
-	public PlayerVO getPlayerdata(String playerName);//根据球员姓名获得球员的数据
+	public PlayerVO getPlayerdata(String season,String playerName);//根据球员姓名获得球员的数据
 	public PlayerinfoVO getPlayerinfo(String playerName);//根据球员姓名获得球员信息
-	public ArrayList<PlayerVO> getAllPlayerdata(String key,String order);//根据条件正序或倒序排列的球员数据的数组
-	public ArrayList<PlayerVO> getFirstFifty(String position,String partition,String key);//筛选前50名
+	public ArrayList<PlayerVO> getAllPlayerdata(String season,String key,String order);//根据条件正序或倒序排列的球员数据的数组
+	public ArrayList<PlayerVO> getFirstFifty(String season,String position,String partition,String key);//筛选前50名
 
 }
