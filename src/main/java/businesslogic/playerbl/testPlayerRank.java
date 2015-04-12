c1164a29fc70765ef1dc32ad90f38e7daf4c7ed1
wangning13 <1287983597@qq.com>
2015-04-12 11:21:01 +0800

    王宁4.12.11.20

--- src/main/java/businesslogic/playerbl/testPlayerRank.java
+++ src/main/java/businesslogic/playerbl/testPlayerRank.java
@@ -17,11 +17,12 @@ public class testPlayerRank {
 //			System.out.println(playerMatchVOs.get(i).getScoring());
 //		}
 	
-		ArrayList<PlayerMatchVO> playerMatchVOs2 = playerRank.getPlayerMonthMatch("13-10", "LAL");
-		for (int i = 0; i < playerMatchVOs2.size(); i++) {
-			System.out.println(playerMatchVOs2.get(i).getPlayername());
-			System.out.println(playerMatchVOs2.get(i).getScoring());
-		}
+//		ArrayList<PlayerMatchVO> playerMatchVOs2 = playerRank.getPlayerMonthMatch("13-10", "Ed Davis");
+//		for (int i = 0; i < playerMatchVOs2.size(); i++) {
+//			System.out.println(playerMatchVOs2.get(i).getPlayername());
+//			System.out.println(playerMatchVOs2.get(i).getMinutes());
+//			System.out.println(playerMatchVOs2.get(i).getScoring());
+//		}
 		
 //		ArrayList<PlayerMatchVO> playerMatchVOs = playerRank.getPlayerRecentFiveMatch("Ed Davis");
 //		for (int i = 0; i < playerMatchVOs.size(); i++) {
@@ -42,7 +43,11 @@ public class testPlayerRank {
 //			System.out.println(a.get(i));
 //		}
 
-		
+		ArrayList<PlayerVO> playerVOs = playerRank.getSeasonTop("12-13", "backboard");
+		System.out.println(playerVOs.size());
+		for (int i = 0; i < playerVOs.size(); i++) {
+			System.out.println(playerVOs.get(i).getMinutes());
+		}
 
 		
 		
